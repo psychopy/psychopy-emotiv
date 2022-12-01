6f84402b75b6af55a4ca302dfc09e835b6f91c9d
mdcutone <matthew.cutone@gmail.com>
2022-12-01 10:16:13 -0500

    FF: Fixed entry points

--- psychopy_emotiv/__init__.py
+++ psychopy_emotiv/__init__.py
@@ -1,1 +1,13 @@
-from .emotiv import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Originally from the PsychoPy library
+# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+__version__ = '0.0.1'
+
+from .emotiv_record import EmotivRecordingComponent
+from .emotiv_marking import EmotivMarkingComponent
+from .emotiv import (
+    Cortex, CortexApiException, CortexTimingException, CortexNoHeadsetException)
